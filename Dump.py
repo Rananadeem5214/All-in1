28ad0336082495e5f40105c77bedcf9c62496024
Rana Nadeem Rajput <83763359+Rananadeem5214@users.noreply.github.com>
2022-11-21 23:03:14 +0500

    Add files via upload

---
+++ Dump.py
@@ -0,0 +1,33 @@
+import os, platform
+
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+    os.system('xdg-open https://www.facebook.com/ITXRANA.5214')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    import Rani64
+    Rani64.kr()
+    #kr()
+
+elif bit == '32bit':
+
+    from dump import p_dump
+
+    p_dump()
+
+else:
+
+    print('\n YOUR DEVICE IS NOT SUPPORT THIS COMMAND')
+
+    os.system('exit')
+    
